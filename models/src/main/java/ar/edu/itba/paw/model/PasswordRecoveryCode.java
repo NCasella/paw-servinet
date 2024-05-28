2e98b42fd641129fec1999b998bef34cda8dfdff
UrielArias <urielarias.a12@gmail.com>
2024-05-27 23:10:18 -0300

    refactor(models, interfaces, persistence, service): migrating PasswordRecoveryCodes to hibernate

--- models/src/main/java/ar/edu/itba/paw/model/PasswordRecoveryCode.java
+++ models/src/main/java/ar/edu/itba/paw/model/PasswordRecoveryCode.java
@@ -1,21 +1,30 @@
 package ar.edu.itba.paw.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "passwordrecoverycodes")
 public class PasswordRecoveryCode {
-    private long userid;
+    @OneToOne(optional = false)
+    private User requestedBy;
+    @Column(nullable = false)
     private UUID code;
+    @Column(nullable = false)
     private LocalDateTime expirationDate;
 
-    public PasswordRecoveryCode(long userid, UUID code, LocalDateTime expirationDate) {
-        this.userid = userid;
+    public PasswordRecoveryCode(User requestedBy, UUID code, LocalDateTime expirationDate) {
+        this.requestedBy = requestedBy;
         this.code = code;
         this.expirationDate = expirationDate;
     }
 
     public long getUserId() {
-        return userid;
+        return requestedBy.getUserId();
     }
 
     public UUID getCode() {
