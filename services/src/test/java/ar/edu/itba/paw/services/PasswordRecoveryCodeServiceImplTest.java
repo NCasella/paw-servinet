5bb224665faacf92ef429fc9ab60bb64a7dc1e39
nicol <ncasella@itba.edu.ar>
2024-06-23 02:25:39 -0300

    test:added passwordRecoveryCodeService test

---
+++ services/src/test/java/ar/edu/itba/paw/services/PasswordRecoveryCodeServiceImplTest.java
@@ -0,0 +1,47 @@
+package ar.edu.itba.paw.services;
+
+import ar.edu.itba.paw.model.PasswordRecoveryCode;
+import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.model.exceptions.UserNotFoundException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PasswordRecoveryCodeServiceImplTest {
+    private static final UUID code=UUID.randomUUID();
+    private static final User userMock=Mockito.mock(User.class);
+
+    @InjectMocks
+    private PasswordRecoveryCodeServiceImpl passwordRecoveryCodeService;
+
+    @Mock
+    private PasswordRecoveryCodeDao passwordRecoveryCodeDao;
+
+    @Mock
+    private UserService userService;
+
+    private static final PasswordRecoveryCode passwordRecoveryCode=Mockito.mock(PasswordRecoveryCode.class);
+
+    @Test(expected = UserNotFoundException.class)
+    public void testCodeForNonExistentUser(){
+        Mockito.when(passwordRecoveryCodeDao.getCodeByUUID(code)).thenReturn(Optional.of(passwordRecoveryCode));
+        Mockito.when(userService.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+        passwordRecoveryCodeService.changePassword(code,"new Pass123");
+        Assert.fail();
+    }
+    @Test
+    public void testCodeValidation(){
+        Mockito.when(passwordRecoveryCodeDao.getCodeByUUID(code)).thenReturn(Optional.of(new PasswordRecoveryCode(userMock,code, LocalDateTime.now().plusHours(1))));
+        boolean validCode= passwordRecoveryCodeService.validateCode(code);
+        Assert.assertTrue(validCode);
+    }
+}
