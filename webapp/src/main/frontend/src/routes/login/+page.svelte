d1d069bb16a35fdcf8f864ac01bb6f418ec0f85a
agussang <msanguinetti@itba.edu.ar>
2025-11-27 10:49:49 -0300

    fix: dark mode is now legible

--- webapp/src/main/frontend/src/routes/login/+page.svelte
+++ webapp/src/main/frontend/src/routes/login/+page.svelte
@@ -29,13 +29,13 @@
 
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">{$t ("login")}</h2>
+    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight">{$t ("login")}</h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
     <form on:submit|preventDefault={handleSubmit} class="space-y-6">
       <div>
-        <label for="email" class="block text-sm/6 font-medium text-gray-900">{$t ("email")}</label>
+        <label for="email" class="block text-sm/6 font-medium ">{$t ("email")}</label>
         <div class="mt-2">
           <input id="email" bind:value={username} type="email" name="email" placeholder={$t("input.email")} required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         </div>
@@ -43,7 +43,7 @@
 
       <div>
         <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">{$t("password")}</label>
+          <label for="password" class="block text-sm/6 font-medium">{$t("password")}</label>
         </div>
         <div class="mt-2">
           <input id="password" bind:value={password} type="password" placeholder={$t("input.password")} name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
@@ -57,20 +57,20 @@
             <p>{$t("login.rememberme")}</p>
           </label>
           <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">{$t("login.forgotpassword")}</a>
+            <a href="#" class="font-semibold text-primary-400">{$t("login.forgotpassword")}</a>
           </div>
         </div>
       </div>
       
       <div>
-        <button type="submit" disabled={loading} class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{$t ("login.submit")}</button>
+        <button type="submit" disabled={loading} class="flex w-full justify-center rounded-md bg-primary-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{$t ("login.submit")}</button>
       </div>
     </form>
 
 
-    <p class="mt-10 text-center text-sm/6 text-gray-500">
+    <p class="mt-10 text-center text-sm/6 text-surface">
       {$t("login.register-1")}
-      <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500 underline">{$t("login.register-2")}</a>
+      <a href="#" class="font-semibold text-primary-400 underline">{$t("login.register-2")}</a>
     </p>
   </div>
 
