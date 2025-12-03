d953262fba934dce39bc8523317f8a9de6652308
Uriel Arias <uarias@itba.edu.ar>
2025-12-03 13:42:41 -0300

    refactor: adding correct mapping for invalid arg0

--- webapp/src/main/java/ar/edu/itba/paw/webapp/mapper/ConstraintValidationExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/mapper/ConstraintValidationExceptionMapper.java
@@ -1,24 +1,49 @@
 package ar.edu.itba.paw.webapp.mapper;
 
-import ar.edu.itba.paw.webapp.dto.ValidationErrorDto;
+import ar.edu.itba.paw.webapp.dto.error.ValidationErrorDto;
 
-import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Provider
 public class ConstraintValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
 
-
     @Override
     public Response toResponse(ConstraintViolationException exception) {
-        List<ValidationErrorDto> errors=exception.getConstraintViolations()
-                .stream().map(constraintViolation -> ValidationErrorDto.fromError(constraintViolation.getMessage(),constraintViolation.getPropertyPath().toString())).toList();
-        return Response.status(Response.Status.BAD_REQUEST).entity(new GenericEntity<>(errors) {}).build();
+        List<ValidationErrorDto> errors = exception.getConstraintViolations()
+                .stream()
+                .map(constraintViolation -> {
+                    String fullPath = constraintViolation.getPropertyPath().toString();
+                    String cleanPath = extractCleanFieldPath(fullPath);
+
+                    return ValidationErrorDto.fromError(
+                            constraintViolation.getMessage(),
+                            cleanPath
+                    );
+                })
+                .toList();
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(new GenericEntity<>(errors) {})
+                .build();
+    }
+
+    private String extractCleanFieldPath(String fullPath) {
+
+        if (fullPath.endsWith(".arg0")) {
+            return "body";
+        }
+
+        int lastDotIndex = fullPath.lastIndexOf('.');
+
+        if (lastDotIndex != -1) {
+            return fullPath.substring(lastDotIndex + 1);
+        }
+
+        return fullPath;
     }
 }
