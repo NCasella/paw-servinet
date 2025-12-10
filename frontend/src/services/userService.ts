e0d42fbbc29ac9c8849f3a225f9691c35566beb9
Uriel Arias <uarias@itba.edu.ar>
2025-12-09 21:39:29 -0300

    chore(frontend): adding services related to user accounts

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,8 +1,13 @@
 import { resetLanguage, setLanguage } from "$lib/i18n/i18n";
 import { User } from "$models/User"
 import { getUser, login, logout } from "$stores/userStore"
-import { GET } from "$utils/apiFetch"
-import { extractUserIdFromToken, extractUserRolesFromToken, removeTokens } from "./authenticate";
+import { GET, POST } from "$utils/apiFetch"
+import { RegisterUserForm } from "$models/forms/UserCreationForm";
+import { getNewIdFromPostResponse } from "$utils/apiFetch";
+import { extractUserIdFromToken, extractUserRolesFromToken, loginWithBasicAuth, removeTokens } from "./authenticate";
+import type { RequestPasswordRecoveryForm } from "$models/forms/RequestPasswordRecoveryForm";
+import type { TResponse } from "$utils/apiFetch";
+import type { ResetPasswordForm } from "$models/forms/ResetPasswordForm";
 
 export async function getUserInfo(id: number ) :Promise<User> {
     const data = await GET(`users/${id}`,{contentType:"user-contact-info" });
@@ -10,6 +15,25 @@ export async function getUserInfo(id: number ) :Promise<User> {
     let user = User.fromJson(data)
     user.setRole( currentUserIsProvider() );
     return user;
+}
+
+export async function createUser(form:RegisterUserForm) :Promise<number> {
+    const response = await POST("users",form, {
+        contentType: "user-registration" 
+    })  
+    return getNewIdFromPostResponse(response)
+}
+
+export async function requestPasswordRecovery(form:RequestPasswordRecoveryForm) : Promise<TResponse> {
+    const response = await POST("users",form, {
+        contentType: "user-password-recovery-request" 
+    })  
+    return response
+}
+
+export async function resetPassword(form:ResetPasswordForm) : Promise<Boolean> {
+    const response = await loginWithBasicAuth(form.code, form.password); 
+    return response
 }
 
 /* Retrieves user login data */
