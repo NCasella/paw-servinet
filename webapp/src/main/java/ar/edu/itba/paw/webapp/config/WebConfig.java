5ccb1fea231a4fb4b63fa706d4209bb0495b5c71
nicol <ncasella@itba.edu.ar>
2025-12-10 17:26:53 -0300

    chore:preparing for deploy

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -70,8 +70,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL94Dialect");
 
         //FIXME: Si ponen esto en prod, hay tabla!!!
-        properties.setProperty("hibernate.show_sql", "true");
-        properties.setProperty("format_sql", "true");
+        //properties.setProperty("hibernate.show_sql", "true");
+        //properties.setProperty("format_sql", "true");
 
         factoryBean.setJpaProperties(properties);
 
