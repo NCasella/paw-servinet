98fe9bfad4618d9063113c48d8f44f10a7ad7f12
nicol <ncasella@itba.edu.ar>
2025-12-16 20:58:47 -0300

    fix: jwt auth edge cases

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/filters/JwtFilter.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/filters/JwtFilter.java
@@ -1,11 +1,8 @@
 package ar.edu.itba.paw.webapp.auth.filters;
 
+import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -34,9 +31,13 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         String token = authorizationHeader.substring("Bearer ".length());
 
-
-        jwtUtil.loginUser(token);
-        chain.doFilter(request,response);
+        try {
+            jwtUtil.loginUser(token);
+        } catch(JwtException | IllegalArgumentException e ) {
+            SecurityContextHolder.clearContext();
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+        chain.doFilter(request, response);
 
     }
 
