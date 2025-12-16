d99908427fedd47c1505853a55e65d0b1cd942be
agussang <msanguinetti@itba.edu.ar>
2025-12-16 12:40:01 -0300

    feat(SPA): added owner validation for businesses /appointments

--- frontend/src/routes/businesses/[id]/appointments/+page.svelte
+++ frontend/src/routes/businesses/[id]/appointments/+page.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
-	import { page } from "$app/state";
 	import AppoinmentList from "$lib/components/appointment/AppoinmentList.svelte";
-	import { getParamIdFromUrl } from "$lib/navigation/pageInfo";
-	import type { Business } from "$models/Business";
 	import { AppointmentView } from "$models/enums/AppointmentStatus";
-	import { getBusinessById } from "$services/businessService";
-	import { onMount } from "svelte";
+	import type { PageData } from "./$types";
 
-    let business :Business
-    onMount( async () => {
-    
-    const businessId = getParamIdFromUrl()
-    business = await getBusinessById(businessId)
-    })
-    
+    export let data :PageData
+    let { business } = data
+
 </script>
 {#if business}
 <AppoinmentList view={AppointmentView.BUSINESS} business={business} />
