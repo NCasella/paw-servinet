d05edcb3eb838ad89935e68c1a88c689539bbe89
agussang <msanguinetti@itba.edu.ar>
2025-12-16 16:23:53 -0300

    feat(SPA): added dynamic page param to url in /businesses/[id]

--- frontend/src/lib/navigation/changePage.ts
+++ frontend/src/lib/navigation/changePage.ts
@@ -1,14 +1,24 @@
 import { goto } from '$app/navigation';
 import { page } from '$app/stores';
+import { error } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 
-export function setPage(newPage: number) {
+export async function setPage(newPage: number) {
   const $page = get(page);
 
   const params = new URLSearchParams($page.url.searchParams);
   params.set('page', newPage.toString());
 
-  goto(`${$page.url.pathname}?${params.toString()}`, {
+  await goto(`${$page.url.pathname}?${params.toString()}`, {
     replaceState: true, // no ensucia el historial
   });
+}
+
+export function readPageFromUrl(url: URL) :number {
+    const page = url.searchParams.get('page') ?? "1";
+    
+      if (Number.isNaN(page)) {
+        throw error(400)//, { message: 'page must be a number' });
+      }
+    return parseInt(page)
 }
--- frontend/src/routes/businesses/[id]/+page.ts
+++ frontend/src/routes/businesses/[id]/+page.ts
@@ -1,21 +1,27 @@
+import { readPageFromUrl } from "$lib/navigation/changePage";
+import { navTo } from "$lib/navigation/pageInfo.js";
 import type { User } from "$models/User.js";
 import { getBusinessById } from "$services/businessService";
 import { getServices } from "$services/serviceService.js";
 import { getCurrentUser } from "$services/userService.js";
 import { error } from "@sveltejs/kit";
 
-export async function load({params, fetch}) {
+export async function load({url, params, fetch}) {
     
+    const pageNum = readPageFromUrl(url)
     try {
         const businessId = parseInt(params.id)
         let business = await getBusinessById(businessId, fetch);
         const user: User | null = await getCurrentUser(fetch).catch(() => null);
         const isOwner = user ? business.isOwner(user.userId) : false;
-        let servicesList = await getServices({ businessId, page: 1 }, fetch);
+        let servicesList = await getServices({ businessId, page: pageNum }, fetch);
+        if ( servicesList.items.length == 0 && pageNum!=1 ) 
+            navTo(`/businesses/${businessId}`)
         return {
             business,
             isOwner,
-            servicesList
+            servicesList,
+            pageNum
         }
     } catch {
         error(404)
