cbb09f8877ddb8272b1e261cc9af15fd9a1db3a3
solrodriguez02 <solrodriguez@itba.edu.ar>
2025-12-17 12:30:32 -0300

    minor fix

--- frontend/src/routes/services/+page.svelte
+++ frontend/src/routes/services/+page.svelte
@@ -12,6 +12,8 @@
 
     import { page as pageStore } from '$app/stores';
     import { goto } from '$app/navigation';
+    import {error} from "@sveltejs/kit";
+    import {StatusCodes} from "$models/exceptions/statusCodesEnum";
 
     let loading = true;
     let page = 1;
@@ -55,7 +57,12 @@
     async function loadServices() {
         loading = true;
         params.page = page;
-        pagedList = await getServices(params);
+        try {
+            pagedList = await getServices(params);
+        } catch (e) {
+            if(e.status === StatusCodes.BAD_REQUEST)
+                console.log("Invalid Params");
+        }
         loading = false;
     }
 
