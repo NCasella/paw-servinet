7ed05fbb2fe8f03194b44ab21b96fdd2efef2592
Uriel Arias <uarias@itba.edu.ar>
2026-02-10 09:54:05 -0300

    fix(webapp.config): unsafe allowedOrigins value

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -123,7 +123,7 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of(SPA_ORIGIN));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.addAllowedHeader("*");
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Authorization-access-token", "Link", "Location", "ETag", "X-Total-Count", "Authorization-Refresh-Token"));
